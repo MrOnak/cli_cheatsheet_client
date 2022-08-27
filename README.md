39fd7eb9a118210b9dfb2c07b9fc46596302acc7
Dominique Stender <oldaric.gw2@gmail.com>
2022-08-27 21:07:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a self-made command-line client to query the https://cht.sh website.
+
+I'm aware that the site has their own client, this here exists as some 
+sort of exercise.
+
---
+++ cht.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+if [[ $# -eq 0 ]]; then
+  echo "Usage:"
+  echo "cht.sh language [keyword]"
+  exit 1
+fi
+
+CHTSH_URL="https://cht.sh/"
+
+if [[ $# -eq 1 ]]; then 
+  curl $CHTSH_URL$1
+elif [[ $# -eq 2 ]]; then
+  curl $CHTSH_URL/$1/$2
+fi
+
